20a4cf918dfe12e1ab7ee2de861e321d29d5ba1a
Harry Steele <harrybsteele@gmail.com>
2023-07-19 15:28:08 -0400

    finished project

--- mathRoutes.js
+++ mathRoutes.js
@@ -5,12 +5,61 @@ const ExpressError = require("./ExpressError");
 app.use(express.json());
 
 app.get("/mean", (req, res, next) => {
+  if (!req.query.nums)
+    throw new ExpressError("query must be comma seperated values", 400);
   const queryNumbers = req.query.nums.split(",");
   const numbers = queryNumbers.map((str) => parseInt(str));
   const mean = numbers.reduce((acc, next) => (acc += next)) / numbers.length;
 
   try {
-    res.json({ operation: "mean", value: `${mean}` });
+    return res.json({ operation: "mean", value: `${mean}` });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get("/median", (req, res, next) => {
+  if (!req.query.nums)
+    throw new ExpressError("query must be comma seperated values", 400);
+  const numbers = req.query.nums
+    .split(",")
+    .map((str) => parseInt(str))
+    .sort((a, b) => a - b);
+  const isEven = numbers.length % 2 === 0;
+  const mid = Math.floor(numbers.length / 2);
+  const middle = !isEven ? numbers[mid] : (numbers[mid - 1] + numbers[mid]) / 2;
+  try {
+    return res.json({ operation: "median", value: `${middle}` });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get("/mode", (req, res, next) => {
+  if (!req.query.nums)
+    throw new ExpressError("query must be comma seperated values", 400);
+
+  const numbers = req.query.nums.split(",").map((str) => parseInt(str));
+
+  try {
+    let count = {};
+    for (let i = 0; i < numbers.length; i++) {
+      if (count[numbers[i]]) {
+        count[numbers[i]] += 1;
+      } else {
+        count[numbers[i]] = 1;
+      }
+    }
+
+    const values = Object.values(count);
+
+    const biggestNum = values.reduce((acc, next) => {
+      return next > acc ? next : acc;
+    }, 0);
+
+    const mode = Object.keys(count).find((num) => count[num] === biggestNum);
+
+    res.json({ operation: "mode", value: mode });
   } catch (e) {
     next(e);
   }
